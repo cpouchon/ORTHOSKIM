296c95d36a8faf453b54f1974eac3cde8d651d8b
charles pouchon <pouchonc@wificampus-039109.grenet.fr>
2019-09-06 13:10:52 +0200

    v.1.0

--- src/ExoGFF_threads.py
+++ src/ExoGFF_threads.py
@@ -57,23 +57,23 @@ def mkdir(path, overwrite=False):
 
 
 class ProgressBar:
-	'''
-	Progress bar
-	'''
-	def __init__ (self, valmax, maxbar, title):
-		if valmax == 0:  valmax = 1
-		if maxbar > 200: maxbar = 200
-		self.valmax = valmax
-		self.maxbar = maxbar
-		self.title  = title
-	def update(self, val):
-		import sys
-		perc  = round((float(val) / float(self.valmax)) * 100)
-		scale = 100.0 / float(self.maxbar)
-		bar   = int(perc / scale)
-		out = '\r %20s [%s%s] %3d %% ' % (self.title, '.' * bar, ' ' * (self.maxbar - bar), perc)
-		sys.stdout.write(out)
-		sys.stdout.flush()
+    '''
+    Progress bar
+    '''
+    def __init__ (self, valmax, maxbar, title):
+        if valmax == 0:  valmax = 1
+        if maxbar > 200: maxbar = 200
+        self.valmax = valmax
+        self.maxbar = maxbar
+        self.title  = title
+    def update(self, val):
+        import sys
+        perc  = round((float(val) / float(self.valmax)) * 100)
+        scale = 100.0 / float(self.maxbar)
+        bar   = int(perc / scale)
+        out = '\r %20s [%s%s] %3d %% ' % (self.title, '.' * bar, ' ' * (self.maxbar - bar), perc)
+        sys.stdout.write(out)
+        sys.stdout.flush()
 
 
 
